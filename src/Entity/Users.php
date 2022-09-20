70a79a4da3a554c1ccc6d4b08836676b49e58c69
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-09-20 17:59:20 +0200

    registration route changed to inscription

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UsersRepository::class)]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Il y a déjà un compte enregistré avec cette adresse email')]
 class Users implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
