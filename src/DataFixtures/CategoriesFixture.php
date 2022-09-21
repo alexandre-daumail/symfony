5001a5da0b885413066a2f54b8a98fac893d4971
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-09-21 15:53:08 +0200

    Category fixure completed

--- src/DataFixtures/CategoriesFixture.php
+++ src/DataFixtures/CategoriesFixture.php
@@ -14,11 +14,21 @@ class CategoriesFixture extends Fixture
     
     public function load(ObjectManager $manager): void
     {
+        $this->createCategory('Biographies', $manager);
+        $this->createCategory('Science Fiction', $manager);
+        $this->createCategory('Pédagogie', $manager);
+        $this->createCategory('Polars', $manager);
+
+        $manager->flush();
+    }
+
+    public function createCategory(string $name, ObjectManager $manager)
+    {
         $category = new Categories();
-        $category->setName('Informatique');
+        $category->setName($name);
         $category->setSlug($this->slugger->slug($category->getName())->lower());
         $manager->persist($category);
 
-        $manager->flush();
+        return $category;
     }
 }
