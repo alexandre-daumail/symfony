5ef49764c5eb005de2996b31e4619c94019ede26
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-09-21 17:00:04 +0200

    Articles Fixtures 1st update

--- src/DataFixtures/ArticlesFixtures.php
+++ src/DataFixtures/ArticlesFixtures.php
@@ -2,15 +2,32 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Articles;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Faker;
+
 
 class ArticlesFixtures extends Fixture
 {
+    public function __construct(private SluggerInterface $slugger){}
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        //Use the Factory to create the fixtures
+        $faker = Faker\Factory::create('fr-FR');
+
+        for($i = 1; $i <= 20; $i++) { 
+
+            $article = new Articles();
+            $article->setTitle($faker->text(5));
+            $article->setContent($faker->text());
+            $article->setSlug($this->slugger->slug($article->getTitle()));
+
+            $manager->persist($article);
+
+        }
 
         $manager->flush();
     }
