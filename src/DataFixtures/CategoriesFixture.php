298c79326ec5394fab3eb802f07eb95f9932c777
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-09-21 13:15:46 +0200

    Category fixture added

---
+++ src/DataFixtures/CategoriesFixture.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Categories;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoriesFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $category = new Categories();
+        $category->setName('Informatique');
+        $category->setSlug('informatique');
+        $manager->persist($category);
+
+        $manager->flush();
+    }
+}
