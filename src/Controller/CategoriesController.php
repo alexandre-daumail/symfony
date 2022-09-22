17e4b8a1de0f944a073d84730bf80253367798bd
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-09-22 18:03:20 +0200

    categoryContr update

---
+++ src/Controller/CategoriesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Categories;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/categories', name: 'categories_')]
+class CategoriesController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('categories/index.html.twig', [
+            'controller_name' => 'CategoriesController',
+        ]);
+    }
+
+    #[Route('/{slug}', name: 'list')]
+    public function list(Categories $category): Response
+    {
+        return $this->render('categories/list.html.twig', compact('category'));
+    }
+
+}
