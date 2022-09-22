9d45bf1dde946cfd3d09048b654cc5a3d6c8c252
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-09-22 11:01:45 +0200

    img fixtures updated

--- src/DataFixtures/ImagesFixtures.php
+++ src/DataFixtures/ImagesFixtures.php
@@ -6,10 +6,10 @@ use App\Entity\Images;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Faker;
 
 use App\DataFixtures\ArticlesFixtures;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Faker;
 
 
 class ImagesFixtures extends Fixture implements DependentFixtureInterface
@@ -21,7 +21,7 @@ class ImagesFixtures extends Fixture implements DependentFixtureInterface
             for($i = 1; $i <= 50; $i++) { 
 
                 $image = new Images();
-                $image->setName($faker->image(null, 640, 480));
+                $image->setName($faker->imageUrl(640, 480, 'animals', true));
 
                 $article = $this->getReference('art-' . rand(1, 20));
                 $image->addArticle($article);
